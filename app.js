ab3c09191969551456af5d3bf61c75cf2d3e72f1
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-13 16:49:29 -0700

    computing the operands

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ class Calculator {
     }
 
     delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
 
     }
 //when the user clicks on the number, it is shown on the screen and multiple numbers can be added
@@ -39,18 +40,46 @@ class Calculator {
     }
 //This is called anytime the user clicks on the operations
     chooseOperation(operation) {
+      //put a checking to prevent operation to go through even when there is no operand
+        if (this.currentOperand === '') return;
+        //add another check
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
         this.operation = operation;
         this.previousOperand = this.currentOperand;
         this.currentOperand = '';
 
     }
     compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+': computation = prev + current;
+                break;
+            case '-': computation = prev - current;
+                break;
+            case '*': computation = prev * current;
+                break;
+            case '÷': computation = prev / current;
+                break;  
+            default:
+                return;       
 
+
+
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     }
 
     updateDisplay() {
 //test if it works
         this.currentOperandTextElement.innerText = this.currentOperand;
+        //moving operand to the top section
         this.previousOperandTextElement.innerText = this.previousOperand;
     }
 
@@ -70,4 +99,19 @@ operationButtons.forEach(button => { button.addEventListener('click', () => {cal
  
     calculator.updateDisplay();
 });
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
 });
